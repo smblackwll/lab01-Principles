388d6896f2a5ec2525c3768df31266699b403536
lifeisanoogie <132381167+lifeisanoogie@users.noreply.github.com>
2024-02-20 16:55:12 -0600

    Button functionality, display updating, two sensor

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
-from time import sleep
-import onewire, ds18x20, time
+from time import sleep, sleep_ms
 import network
-import socket
-import freesans20
-import writer
+import umail
+import onewire
+import ds18x20
 
-""" Set up temp sensors """
+""" Set up network connection """
 
-ds_pin = Pin(16)
- 
-ds_sensor = ds18x20.DS18X20(onewire.OneWire(ds_pin))
- 
-roms = ds_sensor.scan()
+# DeviceNet info
+ssid = 'UI-DeviceNet'
+password = 'UI-DeviceNet'
 
-print('Found DS devices: ', roms)
+def connect_to_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+
+    max_wait = 10
+    while max_wait > 0:
+        if wlan.status() < 0 or wlan.status() >= 3:
+            break
+        max_wait -= 1
+        print('Waiting for connection...')
+        time.sleep(1)
+
+    if wlan.status() != 3:
+        raise RuntimeError('Network connection failed.')
+    else:
+        print('Connected.')
+        status = wlan.ifconfig()
+
+""" Set up buttons """
+
+button1 = Pin(9)
+button2 = Pin(13)
 
 """ Set up OLED """
+
 WIDTH = 128
 HEIGHT = 64
 
@@ -25,89 +45,50 @@ i2c = I2C(1, scl = Pin(3), sda = Pin(2), freq = 200000)
 
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
-def write_to_oled(temp):
+def write_to_oled(sensor_one_status, sensor_two_status, sensor_one_temp, sensor_two_temp):
+    # Convert temps to fahrenheit
+    #   sensor_one_fahrenheit = (sensor_one_temp * (9/5)) + 32
+    #   sensor_two_fahrenheit = (sensor_two_temp * (9/5)) + 32
+
     oled.fill(0)
-    oled.text("TEMP", 0, 0)
-    oled.text(str(temp), 0, 40)
+    oled.text("Sensor 01: " + str(sensor_one_status), 0, 5)
+    oled.text("Sensor 02: " + str(sensor_two_status), 0, 35)
+    if sensor_one_status == "OFF":
+        oled.text("Temp: Not avail", 0, 15)
+    else:
+        oled.text("Temp: " + str(sensor_one_temp) + " C", 0, 15)
+    if sensor_two_status == "OFF":
+        oled.text("Temp: Not avail", 0, 45)
+    else:
+        oled.text("Temp: " + str(sensor_two_temp) + " C", 0, 45)
     oled.show()   
+    sleep(0.1)
 
+""" Set up DS18B20 """
 
-""" Set up webserver """
-def webpage(reading):
-    # Set up HTML template
-    html = f"""<!DOCTYPE html>
-            <html>
-            <head>
-                <title> Temperature </title>
-                <meta http-equiv="refresh" content="10">
-            </head>
-            <body>
-                <p> {reading} </p>
-            </body>
-            </html>
-            """
-    return str(html)
+ds_pin = Pin(16)
+ow = onewire.OneWire(ds_pin)
+ds = ds18x20.DS18X20(ow)
+roms = ds.scan()
+print('Found DS18B20 sensor with address: ', roms)
 
-""" Set up network """
-# Set up AP mode on pico w
-def ap_mode(ssid, password):
-    ap = network.WLAN(network.AP_IF)
-    ap.config(essid=ssid, password=password)
-    ap.active(True)
-
-    while not ap.active():
-        pass
-    print('AP mode is active, you can now connect.')
-    print('IP address to connect to: ' + ap.ifconfig()[0])
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 80))
-    s.listen(5) # listen for 5 because you can only have 5 devices connected to pico w
-
-    prev_temp = None
-
-    while True:
-        try:
-            conn, addr = s.accept()
-            print('Connection from %s' % str(addr))
-            request = conn.recv(1024)
-            print('Content = %s' % str(request))
-
-            ds_sensor.convert_temp()
-            time.sleep_ms(750)
-            temp_reading = round(ds_sensor.read_temp(roms[0]), 2)
-
-            # If temp has changed, update the webpage
-            if temp_reading != prev_temp:
-                response = webpage(temp_reading)
-                prev_temp = temp_reading # Update previous temp
-            else:
-                respone = webpage(prev_temp)
-
-            conn.send(respone)
-        except Exception as e:
-            print("Exception: ", e)
-        finally:
-            conn.close()
-
-""" MAIN """   
-try:
-    ap_mode('NAME', 'PASSWORD')
-except KeyboardInterrupt:
-    machine.reset()
+""" MAIN """
+connect_to_wifi(ssid, password)
 
 while True:
- 
-  ds_sensor.convert_temp()
- 
-  time.sleep_ms(750)
- 
-  for rom in roms:
- 
-    tnum = round(ds_sensor.read_temp(rom), 2)
-    print(tnum, " Celsius")
-    write_to_oled(tnum)
+    ds.convert_temp()
+    sleep_ms(10)
+    # Button 1 is pressed, temp sensor 1 turned on. 
+    if button1.value() == 0:
+        sensor_one_status = "ON"
+        sensor_one_temp = round(ds.read_temp(roms[0]), 1)
+    else: 
+        sensor_one_status = "OFF"
+        
+    if button2.value() == 0:
+        sensor_two_status = "ON"
+        sensor_two_temp = round(ds.read_temp(roms[1]), 1)
+    else: 
+        sensor_two_status = "OFF"
 
-  
- 
-  time.sleep(1)
+    write_to_oled(sensor_one_status, sensor_two_status, sensor_one_temp, sensor_two_temp)

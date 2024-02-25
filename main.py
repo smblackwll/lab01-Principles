db42c5b1c0826bffcfacbda7f936017898ec74fb
lifeisanoogie <132381167+lifeisanoogie@users.noreply.github.com>
2024-02-25 16:56:04 -0600

    Comments added to code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from machine import Pin, I2C
 from ssd1306 import SSD1306_I2C
 from time import sleep, sleep_ms
 import network
-import umail
 import onewire
 import ds18x20
 import urequests
@@ -14,19 +13,22 @@ import json
 ssid = 'UI-DeviceNet'
 password = 'UI-DeviceNet'
 
+# Function for connecting to wifi, ssid = network id and password = network password
 def connect_to_wifi(ssid, password):
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
     wlan.connect(ssid, password)
 
-    max_wait = 10
+    # Time-out after max_wait seconds so that not stuck in endless loop. Print error to screen is not able to connect
+    max_wait = 30
     while max_wait > 0:
         if wlan.status() < 0 or wlan.status() >= 3:
             break
         max_wait -= 1
         print('Waiting for connection...')
-        time.sleep(1)
+        sleep(1)
 
+    # Failed connection if status is 3, otherwise connected
     if wlan.status() != 3:
         raise RuntimeError('Network connection failed.')
     else:
@@ -35,18 +37,23 @@ def connect_to_wifi(ssid, password):
 
 """ Set up buttons """
 
+# Buttons to control temp sensors 1 and 2
 button1 = Pin(9)
 button2 = Pin(13)
 
 """ Set up OLED """
 
+# Pixel width x height
 WIDTH = 128
 HEIGHT = 64
 
+# Set up I2C, (I2C 1 or 0, scl pin number, sda pin number, frequency)
 i2c = I2C(1, scl = Pin(3), sda = Pin(2), freq = 200000)
 
+# Variable for the oled, screen width, height and i2c passed to initialize
 oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)
 
+# Function for writing the temperature and updating to screen
 def write_to_oled(sensor_one_status, sensor_two_status, sensor_one_temp, sensor_two_temp):
     # Convert temps to fahrenheit
     #   sensor_one_fahrenheit = (sensor_one_temp * (9/5)) + 32
@@ -68,11 +75,13 @@ def write_to_oled(sensor_one_status, sensor_two_status, sensor_one_temp, sensor_
 
 """ Set up DS18B20 """
 
+# Defaults
 sensor_one_status = "OFF"
 sensor_two_status = "OFF"
 sensor_one_temp = 0
 sensor_two_temp = 0
 
+# DS_Pin is where the sensors are connected, in this case GPIO16 (pin 21)
 ds_pin = Pin(16)
 ow = onewire.OneWire(ds_pin)
 ds = ds18x20.DS18X20(ow)
@@ -81,32 +90,38 @@ print('Found DS18B20 sensor with address: ', roms)
 
 """ MAIN """
 
-
 while True:
+    # Convert sensor temperature to C
     ds.convert_temp()
-    sleep_ms(1000)
+    sleep_ms(100) # delay to allow conversion
+
     # Button 1 is pressed, temp sensor 1 turned on. 
     if button1.value() == 0:
         try:
             sensor_one_temp = round(ds.read_temp(roms[0]), 1)
             sensor_one_status = "ON"
-        except Exception as e1:
+        except Exception as e1: # user disconnected sensor
             print("Error reading sensor 1: ", e1)
             sensor_one_status = "D/C"
+            sensor_one_temp = -1
     else: 
         sensor_one_status = "OFF"
         sensor_one_temp = 0.0
-            
+    
+    # Button 2 is pressed, temp sensor 2 turned on
     if button2.value() == 0:
         try:
             sensor_two_temp = round(ds.read_temp(roms[1]), 1)
             sensor_two_status = "ON"
-        except Exception as e2:
+        except Exception as e2: # user disconnected sensor
             print("Error reading sensor 2: ", e2)
             sensor_two_status = "D/C"
+            sensor_two_temp = -1
     else: 
         sensor_two_status = "OFF"
         sensor_two_temp = 0.0
+
+    write_to_oled(sensor_one_status, sensor_two_status, sensor_one_temp, sensor_two_temp)
 
     connect_to_wifi(ssid, password)
 
@@ -122,5 +137,3 @@ while True:
     response = urequests.post(url, data=json_data, headers=headers)
     print(response.status_code)
     print(response.text)
-
-    write_to_oled(sensor_one_status, sensor_two_status, sensor_one_temp, sensor_two_temp)
